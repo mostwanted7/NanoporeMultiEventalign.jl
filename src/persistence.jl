7ab195da09f1adad2ad44ccff4157b3cd3d31319
Lazar Davidovic <laki@hotmail.rs>
2021-03-11 21:14:11 +0100

    line number correction

--- src/persistence.jl
+++ src/persistence.jl
@@ -42,7 +42,7 @@ all_lines = readlines(model_path)
 kmers = Dict() # Dictionary to read from all the kmers data
 line_counter = 0 # used to skip first 7 lines of the file
 for line in all_lines
-    if (line_counter > 8)
+    if (line_counter > 6)
         name, mean, stdv = split(line, "\t")
         kmers[name] = mean, stdv
     end
